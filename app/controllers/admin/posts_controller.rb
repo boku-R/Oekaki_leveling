3d69511ecb99108998cf2bb4553548509e1eb60c
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2023-01-27 07:30:13 +0000

    管理者の投稿一覧・投稿詳細を整えました

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,7 +3,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @posts = Post.all.page(params[:page])
+    @posts = Post.all.page(params[:page]).per(10)
     @tag_list = Tag.all
   end
 
