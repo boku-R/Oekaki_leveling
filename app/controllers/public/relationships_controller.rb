fcc3275715707e95172b7d452d76fd4cbe572fef
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2023-02-08 07:35:10 +0000

    フォロー機能を非同期化しました

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,13 +2,13 @@ class Public::RelationshipsController < ApplicationController
   # フォローする
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
   end
 
   # フォローを外す
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
   end
 
   # フォロー一覧
